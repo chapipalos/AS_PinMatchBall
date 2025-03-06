c15142aaecd8ef6e16d3870c58af7e95645e7d1a
chapipalos <alu.151924@usj.es>
2025-03-06 18:05:46 +0100

    Flippers

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -8,23 +8,91 @@ public class Trigger : MonoBehaviour
     private Quaternion originalRotation;
     private bool rotating = false;
 
+    public Transform rightFlipper;
+    public Transform leftFlipper;
+    private Quaternion orgRightRotation;
+    private Quaternion orgLeftRotation;
+
+    private bool leftRotating;
+    private bool rightRotating;
+
+    private bool player;
+
     void Start()
     {
-        originalRotation = transform.rotation; // Store the original rotation
+        //originalRotation = transform.rotation; // Store the original rotation
+        orgLeftRotation = leftFlipper.rotation;
+        orgRightRotation = rightFlipper.rotation;
+
+        if(CompareTag("Player1"))
+        {
+            player = true;
+        }
+        else if(CompareTag("Player2"))
+        {
+            player = false;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rotating = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rotating = false;
-        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    rotating = true;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    rotating = false;
+        //}
 
-        RotateObject();
+        Inputs();
+        
+        RotateFlipper(leftRotating, leftFlipper, orgLeftRotation, false);
+        RotateFlipper(rightRotating, rightFlipper, orgRightRotation, true);
+    }
+
+    private void Inputs()
+    {
+        if (player)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rightRotating = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                rightRotating = false;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                leftRotating = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.S))
+            {
+                leftRotating = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rightRotating = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                rightRotating = false;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                leftRotating = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                leftRotating = false;
+            }
+        }
     }
 
     void RotateObject()
@@ -39,6 +107,29 @@ public class Trigger : MonoBehaviour
         {
             // Return to the original rotation smoothly
             transform.rotation = Quaternion.RotateTowards(transform.rotation, originalRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void RotateFlipper(bool rotate, Transform flipper, Quaternion orRot, bool flip)
+    {
+        if (rotate)
+        {
+            Quaternion targetRotation;
+            // Rotate towards the target rotation angle
+            if (flip)
+            {
+                targetRotation = Quaternion.Euler(0, rotationAngle, 0) * orRot;
+            }
+            else
+            {
+                targetRotation = Quaternion.Euler(0, -rotationAngle, 0) * orRot;
+            }
+            flipper.rotation = Quaternion.RotateTowards(flipper.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Return to the original rotation smoothly
+            flipper.rotation = Quaternion.RotateTowards(flipper.rotation, orRot, rotationSpeed * Time.deltaTime);
         }
     }
 }
