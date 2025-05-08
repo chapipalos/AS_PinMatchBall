d10f2d8d5064034a29a9ae44d69c4a5b620f376d
AdriC03 <132344971+AdriC03@users.noreply.github.com>
2025-05-08 15:49:43 +0200

    particles configured

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,7 +6,8 @@ public class Goal : MonoBehaviour
     private BoxCollider m_BoxCollider;
     public Transform m_RespawnPoint;
     public float m_ForceAmount = 10f;
-
+    public GameObject particles;
+    public GameObject Goalparticles;
 
     void Start()
     {
@@ -17,10 +18,18 @@ public class Goal : MonoBehaviour
     {
         if(other.tag == "BALL")
         {
+            if (Goalparticles != null)
+            {
+                Instantiate(Goalparticles, other.transform.position, Quaternion.identity);
+            }
             Rigidbody rb = other.GetComponent<Rigidbody>();
             rb.Sleep();// Para la velocidad y rotacion 
             other.transform.position = m_RespawnPoint.position;
 
+            if (particles != null)
+            {
+                Instantiate(particles,m_RespawnPoint.position, Quaternion.identity);
+            }
             Vector3 direction = (transform.position - m_RespawnPoint.position).normalized;
             rb.AddForce(direction * m_ForceAmount, ForceMode.Impulse);
 
