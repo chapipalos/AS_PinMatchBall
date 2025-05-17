5f2ef8e9e15e7dfb0a60c0af7b82abfe08214e2e
AdriC03 <132344971+AdriC03@users.noreply.github.com>
2025-05-17 15:51:37 +0200

    Menu y efectos

--- Assets/Scripts/Fan/FanController.cs
+++ Assets/Scripts/Fan/FanController.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class FanController : MonoBehaviour
 {
-    public float rotationSpeed = 90f; // Degrees per second
+    public float rotationSpeed = 90f;
     private float m_CurrentSpeed;
 
     public float m_FanForce;
@@ -13,13 +14,25 @@ public class FanController : MonoBehaviour
     private GameObject m_WindEffect;
     private ParticleSystem m_WindParticleSystem;
 
+    public GameObject m_WindHitEffectPrefab;
+    private GameObject m_WindHitEffect;
+    private ParticleSystem[] m_WindHitParticles;
+
     private bool m_Effect;
+
+    private HashSet<Collider> m_AlreadyTriggered = new HashSet<Collider>();
 
     private void Awake()
     {
+ 
         m_WindEffect = Instantiate(m_WindPrefab);
         m_WindEffect.transform.position = m_Blades.transform.position;
         m_WindParticleSystem = m_WindEffect.GetComponent<ParticleSystem>();
+
+  
+        m_WindHitEffect = Instantiate(m_WindHitEffectPrefab);
+        m_WindHitEffect.SetActive(false);
+        m_WindHitParticles = m_WindHitEffect.GetComponentsInChildren<ParticleSystem>();
     }
 
     private void Start()
@@ -36,8 +49,8 @@ public class FanController : MonoBehaviour
             m_CurrentSpeed += rotationSpeed * dt;
             m_CurrentSpeed = Mathf.Clamp(m_CurrentSpeed, 0f, rotationSpeed);
             m_Blades.Rotate(Vector3.forward, m_CurrentSpeed);
-            Debug.Log("Rotating Fan");
-            if(!m_Effect)
+
+            if (!m_Effect)
             {
                 m_WindParticleSystem.Play();
                 m_Effect = true;
@@ -48,7 +61,7 @@ public class FanController : MonoBehaviour
             m_CurrentSpeed -= rotationSpeed * dt;
             m_CurrentSpeed = Mathf.Clamp(m_CurrentSpeed, 0f, rotationSpeed);
             m_Blades.Rotate(Vector3.forward, m_CurrentSpeed);
-            Debug.Log("No Rotating Fan");
+
             if (m_Effect)
             {
                 m_WindParticleSystem.Stop();
@@ -56,22 +69,54 @@ public class FanController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            GameManager.m_FanRotating = true;
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            GameManager.m_FanRotating = false;
-        }
+        if (Input.GetKeyDown(KeyCode.J)) GameManager.m_FanRotating = true;
+        if (Input.GetKeyDown(KeyCode.K)) GameManager.m_FanRotating = false;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (GameManager.m_FanRotating && other.CompareTag("BALL"))
         {
-            float distance = Vector3.Distance(other.gameObject.transform.position, transform.position);
-            other.gameObject.GetComponent<Rigidbody>().AddForce(-transform.right * (1/distance) * m_FanForce, ForceMode.Impulse);
+            float distance = Vector3.Distance(other.transform.position, transform.position);
+            other.attachedRigidbody?.AddForce(-transform.right * (1 / distance) * m_FanForce, ForceMode.Impulse);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameManager.m_FanRotating && other.CompareTag("BALL") && !m_AlreadyTriggered.Contains(other))
+        {
+            m_AlreadyTriggered.Add(other);
+
+            if (m_WindHitEffect != null)
+            {
+               
+                m_WindHitEffect.SetActive(true);
+
+                foreach (var ps in m_WindHitParticles)
+                {
+                    ps.transform.position = other.transform.position;
+                    ps.Play();
+                }
+
+                
+                Invoke(nameof(DeactivateWindHitEffect), 2f);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_AlreadyTriggered.Remove(other);
+    }
+
+    private void DeactivateWindHitEffect()
+    {
+        foreach (var ps in m_WindHitParticles)
+        {
+            ps.Stop();
+        }
+        m_WindHitEffect.SetActive(false);
+    }
 }
+
