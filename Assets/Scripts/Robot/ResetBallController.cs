38048f107f8aed4d8753472c38810293532c0198
chapipalos <alu.151924@usj.es>
2025-05-19 16:56:16 +0200

    prueba

--- Assets/Scripts/Robot/ResetBallController.cs
+++ Assets/Scripts/Robot/ResetBallController.cs
@@ -1,20 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class ResetBallController : MonoBehaviour
 {
     public Transform m_ResetBall;
 
+    private IEnumerator m_ResetBallCoroutine;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("BALL"))
         {
             other.attachedRigidbody.linearVelocity = Vector3.zero;
-            Invoke("ResetBall", 2f);
+            m_ResetBallCoroutine = ResetBall(other.transform);
         }
     }
 
-    private void ResetBall(Transform ball)
+    private IEnumerator ResetBall(Transform ball)
     {
+        yield return new WaitForSeconds(2f);
         ball.position = m_ResetBall.position;
     }
 }
